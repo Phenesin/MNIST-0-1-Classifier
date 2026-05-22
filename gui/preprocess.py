95a0d4fbc96e3b0290085d552aeb920a4f0aba0c
Dotachin <siddharthamanu5@gmail.com>
2026-05-22 16:49:25 +0530

    Improving the performance

--- gui/preprocess.py
+++ gui/preprocess.py
@@ -13,12 +13,21 @@ def preprocess_image(image, device):
 
         image_array = image_array[y_min : y_max + 1, x_min : x_max + 1]
 
-    image = Image.fromarray(image_array)
-    image = image.resize((28, 28))
+    cropped_image = Image.fromarray(image_array)
+    width, height = cropped_image.size
+
+    if width > height:
+        new_width = 20
+        new_height = int((height / width) *20)
+    else:
+        new_height = 20
+        new_width = int((height / width) *20)
+
     final_image = Image.new("L", (28, 28), 0)
-    final_image.paste(image, (4, 4))
+    paste_x = (28 - new_width) // 2
+    paste_y = (28 - new_height) // 2
     transform = transforms.ToTensor()
-    tensor = transform(image)
-    tensor = tensor.unsqueeze(0)
+    tensor = transform(final_image)
+    tensor = tensor.view(1, 784)
     tensor = tensor.to(device)
     return tensor, final_image
